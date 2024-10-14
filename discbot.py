4b755e94f4f706ec7001be6e52aaf6824e3fa970
nicolas2110 <116236637+nicolas2110@users.noreply.github.com>
2024-10-14 02:46:48 -0500

    Update discbot.py

--- discbot.py
+++ discbot.py
@@ -1,8 +1,9 @@
+import os
 import discord
 from discord.ext import commands
 import yt_dlp
 import asyncio
-
+DISC_TOKEN = os.getenv('DISC_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
